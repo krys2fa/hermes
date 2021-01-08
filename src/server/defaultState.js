17f4594fda51ebf322f1d6aef7f3a289393e4139
Christopher Amanor <kriskit7@gmail.com>
2021-01-08 22:17:00 +0000

    Add redux and redux store to the application

---
+++ src/server/defaultState.js
@@ -0,0 +1,44 @@
+export const defaultState = {
+  users: [
+    {
+      id: 'U1',
+      name: 'Dev',
+    },
+    {
+      id: 'U2',
+      name: 'C. Eeyo',
+			// passwordHash: md5("TUPLES")
+    },
+  ],
+  groups: [
+    {
+      name: 'To Do',
+      id: 'G1',
+      owner: 'U1',
+    },
+  ],
+  tasks: [
+    {
+      name: 'Do tests',
+      id: 'T1',
+      group: 'G2',
+      owner: 'U2',
+      isComplete: false,
+    },
+    {
+      name: 'Write tests',
+      id: 'T1',
+      group: 'G1',
+      owner: 'U1',
+      isComplete: false,
+    },
+  ],
+  comments: [
+    {
+      owner: 'U1',
+      id: 'C1',
+      task: 'T1',
+      content: 'Great work!!!',
+    },
+  ],
+};
