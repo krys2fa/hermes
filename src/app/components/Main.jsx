4e4846fdabfa1d88884f80c1ee17d7cfd8210e0b
Christopher Amanor <kriskit7@gmail.com>
2021-01-13 14:05:38 +0000

    Add navigation to app

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
+import { Router, Route } from 'react-router-dom';
+import { history } from '../store/history';
 import { ConnectedDashboard } from './Dashboard';
+import { ConnectedNavigation } from './Navigation';
+import { ConnectedTaskList } from './TaskList';
 
 export const Main = () => (
-	<Provider store={store}>
-		<div>
-			Dashboard Goes Here
-			<ConnectedDashboard />
-		</div>
-	</Provider>
+	<Router history={history}>
+	  <Provider store={store}>
+			<div>
+				{/* Dashboard Goes Here
+				 */}
+				<ConnectedNavigation />
+				<Route exact path="/dashboard" render={() => (<ConnectedDashboard />)} />
+			</div>
+		</Provider>
+	</Router>
 )
 
